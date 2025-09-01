cd2b9444c2217ef51a77568400d33d4d58d0fe7b
Mathias San Miguel <enlovson@amazon.com>
2025-09-01 14:56:42 +0000

    [zsh] reduce warning noice for missing runtime configs for pyenv, etc

--- _home/.env/profile/70_runtimes.sh
+++ _home/.env/profile/70_runtimes.sh
@@ -2,41 +2,42 @@
 
 # NOTE: Consider mise: https://mise.jdx.dev/dev-tools/
 
+_config_warnings=()
 
 # Python
 # -----------------------------------------------------------------------------
-if which pyenv > /dev/null; then
+if [[ -x "$(command -v pyenv)" ]]; then
     eval "$(pyenv init - "$USING_SHELL")"
     # eval "$(pyenv virtualenv-init -)" # Makes prompt SLOW
     export PATH="$PYENV_ROOT/bin:$PATH"
 else
-    echo "Missing 'pyenv', skipping its config" 1>&2
+    _config_warnings+=pyenv
 fi
 
 
 # Node
 # -----------------------------------------------------------------------------
-if which nodenv > /dev/null; then 
+if [[ -x "$(command -v nodenv)" ]]; then 
     export PATH="$NODENV_ROOT/bin:$PATH"
     eval "$(nodenv init -)"
 else 
-    echo "Missing 'nodenv', skipping its config" 1>&2
+    _config_warnings+=nodenv
 fi
 
 if [ -s "$NVM_DIR/nvm.sh" ]; then 
     . "$NVM_DIR/nvm.sh"
 else 
-    echo "Missing 'nvm' script, skipping its config" 1>&2
+    _config_warnings+=nvm
 fi
 
 
 # Perl
 # -----------------------------------------------------------------------------
-if which plenv > /dev/null; then 
+if [[ -x "$(command -v plenv)" ]]; then 
     export PATH="$PLENV_ROOT/bin:$PATH"
     eval "$(plenv init - zsh)"
 else 
-    echo "Missing 'plenv', skipping its config" 1>&2
+    _config_warnings+=plenv
 fi
 
 
@@ -45,6 +46,15 @@ fi
 if [ -s "$CARGO_HOME/env" ]; then
     source "$CARGO_HOME/env"
 else
-    echo "Missing 'cargo', skipping its config" 1>&2
+    _config_warnings+=cargo
 fi
 
+
+# Finally: log warnings
+# =============================================================================
+YELLOW='\033[0;33m'
+YELLOW_BOLD='\033[1;33m'
+NC='\033[0m' # No Color
+printf -v _config_warnings "<%s>," "${_config_warnings[@]}"
+echo -en "${YELLOW_BOLD}Warning! Skipped configurations: " 1>&2
+echo -e "${YELLOW}Missing ${_config_warnings%,}.${NC}\n" 1>&2
