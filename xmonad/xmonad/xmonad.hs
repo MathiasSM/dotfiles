46f7c3b2a9fdbe4e422363ad52d4845fe21a6cc3
Mathias San Miguel <mathiassanmiguel@gmail.com>
2025-09-07 05:12:41 +0200

    [xmonad][xmobar] Added missing configurations and cabal env file

--- xmonad/xmonad/xmonad.hs
+++ xmonad/xmonad/xmonad.hs
@@ -48,12 +48,12 @@ myIconFmt wid wins = iconsFmtAppend concat wid wins
 
 myIconConfig :: IconConfig
 myIconConfig = def
-	{ iconConfigIcons = myIcons
-	, iconConfigFmt = myIconFmt
-	, iconConfigFilter = iconsGetFocus
-	}
+    { iconConfigIcons = myIcons
+    , iconConfigFmt = myIconFmt
+    , iconConfigFilter = iconsGetFocus
+    }
 
-barSpawner :: ScreenId -> IO StatusBarConfig
+barSpawner :: ScreenId -> X StatusBarConfig
 barSpawner screen = pure $ xmobar_n screen
 
 xmobar_n (S n) = statusBarPropTo ("_XMONAD_LOG") ("xmobar -x " ++ show n) myXmobarPP
@@ -64,7 +64,7 @@ myXmobarPP = dynamicIconsPP myIconConfig $ def
     , ppTitle           = id
     , ppTitleSanitize   = id
     , ppCurrent         = white . xmobarBorder "Top" "#8be9fd" 3
-    , ppVisible	        = white
+    , ppVisible         = white
     , ppVisibleNoWindows= Just (const "")
     , ppHidden          = grey
     , ppHiddenNoWindows = const ""
@@ -83,24 +83,24 @@ myXmobarProp = dynamicEasySBs barSpawner
 
 
 myAdditionalKeys = 
-	[ ("<XF86AudioLowerVolume>", spawn "amixer sset Master 5%-")
-	, ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 5%+")
-	, ("<XF86AudioMute>", spawn "amixer sset Master toggle")
-	, ("M-f", selectWindow myEasyMotionConfig >>= (`whenJust` windows . focusWindow))
-	, ("M-d", selectWindow myEasyMotionConfig >>= (`whenJust` killWindow))
-	, ("M-/", spawn "get-active-xprops.sh")
-	, ("M-p", spawn "rofi -show")
-	, ("M-0", spawn "toggle-compositor.sh")
-	]
+    [ ("<XF86AudioLowerVolume>", spawn "amixer sset Master 5%-")
+    , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 5%+")
+    , ("<XF86AudioMute>", spawn "amixer sset Master toggle")
+    , ("M-f", selectWindow myEasyMotionConfig >>= (`whenJust` windows . focusWindow))
+    , ("M-d", selectWindow myEasyMotionConfig >>= (`whenJust` killWindow))
+    , ("M-/", spawn "get-active-xprops.sh")
+    , ("M-p", spawn "rofi -show")
+    , ("M-0", spawn "toggle-compositor.sh")
+    ]
 
 myEasyMotionConfig :: EasyMotionConfig
 myEasyMotionConfig = def
-	{ sKeys = PerScreenKeys $ StrictMap.fromList $
-		[ (0, [xK_f, xK_d, xK_s, xK_a])
-		, (1, [xK_h, xK_j, xK_k, xK_l])
-		]
-	, cancelKey = xK_Escape
-	}
+    { sKeys = PerScreenKeys $ StrictMap.fromList $
+        [ (0, [xK_f, xK_d, xK_s, xK_a])
+        , (1, [xK_h, xK_j, xK_k, xK_l])
+        ]
+    , cancelKey = xK_Escape
+    }
 
 --------------------------------------------------------------------------------
 
@@ -108,28 +108,28 @@ myRandrChangeHook :: X()
 myRandrChangeHook = spawn "autorandr --change"
 
 myAutorandrHook = rescreenHook $ def
-	{ afterRescreenHook = spawn "autorandr --change"
-	, randrChangeHook = spawn "autorandr --change" }
+    { afterRescreenHook = spawn "autorandr --change"
+    , randrChangeHook = spawn "autorandr --change" }
 
 myFadeHook :: FadeHook
 myFadeHook = composeAll
-	[ opaque
-	, isUnfocused --> opacity 0.9
-	, isFloating --> opacity 1
-	]
-	
+    [ opaque
+    , isUnfocused --> opacity 0.9
+    , isFloating --> opacity 1
+    ]
+    
 myLogHook = fadeWindowsLogHook myFadeHook
 
 myManageHook = composeAll
-	[ className =? "Steam"                        --> doCenterFloat
-	, className =? "Gimp"                         --> doCenterFloat
-	, className =? "MPlayer"                      --> doCenterFloat
-	, className =? "Pavucontrol"                  --> doCenterFloat
-	, className =? "firefox" <&&> appName =? "Toolkit" --> doCenterFloat
-	, className =? "Xfce4-power-manager-settings" --> doCenterFloat
-	, isFullscreen        --> doFullFloat
-	, isDialog            --> doFloat
-	]
+    [ className =? "Steam"                        --> doCenterFloat
+    , className =? "Gimp"                         --> doCenterFloat
+    , className =? "MPlayer"                      --> doCenterFloat
+    , className =? "Pavucontrol"                  --> doCenterFloat
+    , className =? "firefox" <&&> appName =? "Toolkit" --> doCenterFloat
+    , className =? "Xfce4-power-manager-settings" --> doCenterFloat
+    , isFullscreen        --> doFullFloat
+    , isDialog            --> doFloat
+    ]
 
 myEventHook = fadeWindowsEventHook
 -- TODO: Screen corners?
@@ -140,20 +140,20 @@ myStartupHook = setDefaultCursor xC_left_ptr
 
 main :: IO ()
 main = xmonad
-	. ewmhFullscreen 
-	. ewmh 
-	. myAutorandrHook
-	. myXmobarProp 
-	. docks
-	$ def 
-		{ focusFollowsMouse = True
-		, clickJustFocuses = False
-		, normalBorderColor = "#555555"
-		, focusedBorderColor = "#F05050"
-		, manageHook = myManageHook
-		, logHook = myLogHook
-		, handleEventHook = myEventHook
-		, startupHook = myStartupHook
-		, terminal = "alacritty"
-		} `additionalKeysP` myAdditionalKeys
-		
+    . ewmhFullscreen 
+    . ewmh 
+    . myAutorandrHook
+    . myXmobarProp 
+    . docks
+    $ def 
+        { focusFollowsMouse = True
+        , clickJustFocuses = False
+        , normalBorderColor = "#555555"
+        , focusedBorderColor = "#F05050"
+        , manageHook = myManageHook
+        , logHook = myLogHook
+        , handleEventHook = myEventHook
+        , startupHook = myStartupHook
+        , terminal = "alacritty"
+        } `additionalKeysP` myAdditionalKeys
+        
