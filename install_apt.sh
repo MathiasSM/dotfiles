ec8b2c57906c30a877d94d7e795f69315e068e50
Mathias San Miguel <mathiassanmiguel@gmail.com>
2025-10-06 00:46:49 +0200

    [install] Added debian packages

--- install_apt.sh
+++ install_apt.sh
@@ -9,7 +9,73 @@ source "$DOTFILES/utils.sh"
 # ==============================================================================
 
 APT_PACKAGES=(
+    # Required for life
+    "git"
+    "git-doc"
+
+    # Required for install_flatpak
     "flatpak"
+    "flatpak-xdg-utils"
+    # Required for linking
+    "stow"
+
+    # My xsession
+    "arandr"
+    "autorandr"
+    "dunst"
+    "feh"
+    "picom"
+    "rofi"
+    "scrot"
+    "tmux"
+    "variety"
+    "xfce4-power-manager"
+    "xfce4-power-manager-plugins"
+    "xsecurelock"
+    "xserver-xorg-input-libinput"
+    "xss-lock"
+    "zsh"
+    "zsh-doc"
+
+    # Docs
+    "manpages"
+    "manpages-dev"
+    "manpages-posix"
+    "manpages-posix-dev"
+
+    # My basics
+    "alacritty"
+    "caffeine"
+    "mpv"
+    "mpv-mpris"
+
+    # Tools
+    "bat"
+    "ffmpeg"
+    "ffmpeg-doc"
+    "gpg"
+    "graphviz"
+    "graphviz-doc"
+    "htop"
+    "imagemagick"
+    "imagemagick-doc"
+    "iotop"
+    "jq"
+    "lynx"
+    "make"
+    "make-doc"
+    "pandoc"
+    "pgcli"
+    "powertop"
+
+    # Basic fonts
+    "fonts-kanjistrokeorders"
+    "fonts-mathjax"
+    "fonts-noto"
+    "fonts-noto-cjk"
+    "fonts-open-sans"
+    "fonts-opendyslexic"
+    "fonts-opensymbol"
 )
 
 # ------------------------------------------------------------------------------
